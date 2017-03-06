b79e0fddaa7f566947bbb3bc5badfc38bc4e9134
susanwang <wangjm323@gmail.com>
2017-03-06 20:54:46 +0000

    Create edit template and action

--- workspace/app/controllers/articles_controller.rb
+++ workspace/app/controllers/articles_controller.rb
@@ -3,16 +3,30 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     def create
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
         # @article.save
         # redirect_to article_path(@article)
         if @article.save
-            flash[:notice] = "The article successfully created"
+            flash[:notice] = "The article is successfully created"
             redirect_to article_path(@article)
         else
             render 'new'
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "The article is successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
         end
     end
     
